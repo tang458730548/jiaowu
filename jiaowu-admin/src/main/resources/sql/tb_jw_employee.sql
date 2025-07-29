38b7e72dfb0cd0128835b1bd3fe510b954e3de3a
tsc <458730548@qq.com>
2025-07-29 17:06:47 +0800

    增加职工表

---
+++ jiaowu-admin/src/main/resources/sql/tb_jw_employee.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `tb_jw_employee` (
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `username` VARCHAR(50) NOT NULL COMMENT '用户名',
+  `password` VARCHAR(100) NOT NULL COMMENT '密码',
+  `gender` CHAR(1) DEFAULT NULL COMMENT '性别',
+  `nickname` VARCHAR(50) DEFAULT NULL COMMENT '昵称',
+  `title` VARCHAR(50) DEFAULT NULL COMMENT '职称',
+  `email` VARCHAR(100) DEFAULT NULL COMMENT '邮箱',
+  `enroll_year` INT(4) DEFAULT NULL COMMENT '入学年份',
+  `status` TINYINT(1) DEFAULT 1 COMMENT '状态 1-正常 0-禁用',
+  `create_time` DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='教务员工表'; 
